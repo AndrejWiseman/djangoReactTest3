e3a3f957516d63f9b60a11f858ef5e7459e562a1
A.M. <andrej2000aa@gmail.com>
2023-11-16 12:14:46 +0100

    novi sa frontend

--- proba/views.py
+++ proba/views.py
@@ -1,7 +1,20 @@
+from django.shortcuts import render
 from .models import Film
 from .serializers import FilmSerializer
 from rest_framework import generics
 
+
+
+def front(request):
+    context = {
+
+    }
+    return render(request, "index.html", context)
+
+
+
 class FilmListCreate(generics.ListCreateAPIView):
     queryset = Film.objects.all()
     serializer_class = FilmSerializer
+
+
