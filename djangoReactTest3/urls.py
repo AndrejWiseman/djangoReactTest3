7f089b04b92d1ef6daa4d2d0ac43952aecc6cf1d
A.M. <andrej2000aa@gmail.com>
2023-11-16 11:54:08 +0100

    novi sa frontend

--- djangoReactTest3/urls.py
+++ djangoReactTest3/urls.py
@@ -2,6 +2,8 @@
 from django.contrib import admin
 from django.urls import path, include
 from core.views import front
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -10,3 +12,6 @@ urlpatterns = [
 
     path("", front, name="front"),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
